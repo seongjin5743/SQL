4010abb6398563bfee1de3a15a45a7d585d2c0c7
seongjin <seongjin5743@naver.com>
2025-04-04 10:00:12 +0900

    qwer

---
+++ sqlite3/3.update.sql
@@ -0,0 +1,6 @@
+-- movie = Movie.objects.get(id=1)
+-- movie.title = 'spiderman'
+-- movie.save()
+UPDATE movies_movie
+SET title = 'spiderman'
+WHERE id = 1;
---
+++ sqlite3/4.delete.sql
@@ -0,0 +1,4 @@
+-- movie = Movie.objects.get(id=1)
+-- movie.delete()
+DELETE FROM movies_movie
+WHERE id = 1;
